cb5571b917ea4032f8f31206344185e4048d970b
Jack Swindell <jackson1swindell@gmail.com>
2024-09-26 10:11:30 -0500

    Create DatabaseInitialization.sql

---
+++ src/main/resources/sql/DatabaseInitialization.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS Songs (
+  song_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT NOT NULL,
+  artist TEXT NOT NULL,
+  url TEXT NOT NULL,
+  downloaded INTEGER NOT NULL
+);
+CREATE TABLE IF NOT EXISTS Playlists (
+  playlist_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS PlaylistSongs (
+  playlist_id INTEGER NOT NULL,
+  song_id INTEGER NOT NULL,
+  PRIMARY KEY (playlist_id, song_id),
+  FOREIGN KEY (playlist_id) REFERENCES Playlists(playlist_id) ON DELETE CASCADE,
+  FOREIGN KEY (song_id) REFERENCES Songs(song_id) ON DELETE CASCADE
+);
+CREATE TABLE IF NOT EXISTS RecentlyPlayed (
+  song_id INTEGER NOT NULL,
+  last_listen DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (song_id) REFERENCES Songs(song_id) ON DELETE CASCADE
+);
