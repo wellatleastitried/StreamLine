20cc49f7bf83919580f1a2884e34005178f22f49
Jack Swindell <jackson1swindell@gmail.com>
2024-09-26 11:14:19 -0500

    Update UpdateRecentlyPlayed.sql

--- src/main/resources/sql/UpdateRecentlyPlayed.sql
+++ src/main/resources/sql/UpdateRecentlyPlayed.sql
@@ -1,1 +1,2 @@
+-- DO NOT MODIFY THIS FILE --
 DELETE FROM RecentlyPlayed WHERE song_id IN (SELECT song_id FROM RecentlyPlayed ORDER BY last_listen ASC LIMIT (SELECT COUNT(*) FROM RecentlyPlayed) - 15);
