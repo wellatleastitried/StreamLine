fe211a591d6ae6ee9fdf06c71585efdbcd1210ae
Jack Swindell <jackson1swindell@gmail.com>
2025-03-15 17:11:02 -0500

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+# PREPARE BASH SCRIPTS
 cd scripts/ || exit
 echo "Marking all bash scripts as executable..."
 
@@ -14,3 +16,16 @@ do
 done
 
 echo "All bash scripts have been marked as executable!"
+
+
+# PREPARE CONFIGURATION FILE
+APP_DIR=~/.local/share/StreamLine/config
+if [ ! -f "$APP_DIR"/config.properties ]; then
+    echo 'language=en' > "$APP_DIR"/config.properties
+    echo "config.properties has been created and the language for the app has been set to English."
+fi
+
+if [ ! -f "$APP_DIR"/tinylog.properties ]; then
+    printf "writer = file\nwriter.file = /tmp/StreamLine/streamline.log\nlevel = debug\nformat = {class_name}#{method:-unknown}(): {message}\nwritingmode = overwrite" > "$APP_DIR"/tinylog.properties
+    echo "tinylog.properties has been created."
+fi
