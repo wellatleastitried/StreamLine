e6e0212ddfaf818c5b59238ab16be5cf1d71dd0c
wellatleastitried <jackson1swindell@gmail.com>
2025-05-17 20:08:24 -0500

    Adding multi-page support for LikedMusicPage.java

--- src/main/java/com/streamline/frontend/terminal/pages/LikedMusicPage.java
+++ src/main/java/com/streamline/frontend/terminal/pages/LikedMusicPage.java
@@ -11,8 +11,10 @@ import com.streamline.utilities.LanguagePeer;
 
 import java.io.IOException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -29,22 +31,35 @@ public class LikedMusicPage extends BasePage {
 
     private final TextGUI textGUI;
 
+    private final BasicWindow window;
     private final Panel panel;
 
-    private Panel resultsBox;
+    private final List<Panel> resultPanels;
+
+    private final Button backButton;
+    private final Button pageUpButton;
+    private final Button pageDownButton;
+
     private final int RESULT_PANEL_WIDTH;
     private final int RESULT_PANEL_HEIGHT;
-
     private final int SONG_BUTTON_WIDTH;
     private final int SONG_BUTTON_HEIGHT;
 
     private Map<Integer, Button> likedMusicButtons;
+    private Set<Button> currentButtons = new LinkedHashSet<>();
+
+    private int CURRENT_PAGE = 0;
+    private int CURRENT_PAGE_BACKEND = 0;
 
     public LikedMusicPage(TerminalWindowManager windowManager, Dispatcher backend, TextGUIThread guiThread, TerminalComponentFactory componentFactory, TextGUI textGUI) {
         super(windowManager, backend, guiThread, componentFactory);
         this.textGUI = textGUI;
-
+        this.window = createStandardWindow(LanguagePeer.getText("window.likedMusicTitle"));
         this.panel = componentFactory.createStandardPanel();
+        this.resultPanels = new ArrayList<>();
+        this.pageUpButton = createPageChangeButton("up");
+        this.pageDownButton = createPageChangeButton("down");
+        this.backButton = createPageChangeButton("back");
         this.RESULT_PANEL_WIDTH = componentFactory.getTerminalSize().getColumns();
         this.RESULT_PANEL_HEIGHT = componentFactory.getTerminalSize().getRows() - panel.getSize().getRows() - 15;
         this.SONG_BUTTON_WIDTH = RESULT_PANEL_WIDTH;
@@ -54,42 +69,95 @@ public class LikedMusicPage extends BasePage {
     @Override
     public BasicWindow createWindow() {
         likedMusicButtons = new HashMap<Integer, Button>();
-
-        BasicWindow window = createStandardWindow(LanguagePeer.getText("window.likedMusicTitle"));
+        resultPanels.clear();
 
         panel.addComponent(componentFactory.createEmptySpace());
         panel.addComponent(componentFactory.createLabel(LanguagePeer.getText("label.likedMusicTitle")));
 
-        resultsBox = new Panel();
-        resultsBox.setLayoutManager(new GridLayout(1));
-        resultsBox.setPreferredSize(new TerminalSize(RESULT_PANEL_WIDTH, RESULT_PANEL_HEIGHT));
-        resultsBox.setFillColorOverride(TextColor.ANSI.BLACK_BRIGHT);
+        CURRENT_PAGE_BACKEND = 0;
+        resultPanels.add(new Panel());
+        resultPanels.get(CURRENT_PAGE_BACKEND).setLayoutManager(new GridLayout(1));
+        resultPanels.get(CURRENT_PAGE_BACKEND).setPreferredSize(new TerminalSize(RESULT_PANEL_WIDTH, RESULT_PANEL_HEIGHT));
+        resultPanels.get(CURRENT_PAGE_BACKEND).setFillColorOverride(TextColor.ANSI.BLACK_BRIGHT);
 
-        Set<Button> currentButtons = new LinkedHashSet<>();
         panel.addComponent(componentFactory.createEmptySpace());
-        handleSongRendering(currentButtons);
-        panel.addComponent(resultsBox);
+
+        if (currentButtons.size() > 20 && CURRENT_PAGE > 0) {
+            panel.addComponent(pageUpButton);
+            panel.addComponent(componentFactory.createEmptySpace());
+        }
+        handleSongRendering();
+        panel.addComponent(resultPanels.get(CURRENT_PAGE_BACKEND));
+        panel.addComponent(componentFactory.createEmptySpace());
+
+        if (currentButtons.size() > 20 && CURRENT_PAGE < currentButtons.size() - (resultPanels.size() * 20)) {
+            panel.addComponent(pageDownButton);
+            panel.addComponent(componentFactory.createEmptySpace());
+        }
 
         /* Back button */
-        panel.addComponent(componentFactory.createButton(
-                    LanguagePeer.getText("button.back"), 
-                    () -> windowManager.returnToMainMenu(window),
-                    componentFactory.getButtonWidth() / 3, 
-                    componentFactory.getButtonHeight() / 2
-                    ));
+        panel.addComponent(backButton);
 
         window.setComponent(panel);
         return window;
     }
+    private Button createPageChangeButton(String direction) {
+        if ("up".equals(direction)) {
+            return componentFactory.createButton(
+                    LanguagePeer.getText("button.pageUp"), 
+                    () -> {
+                        for (Button button : currentButtons) {
+                            resultPanels.get(CURRENT_PAGE_BACKEND).removeComponent(button);
+                        }
+                        currentButtons.clear();
+                        CURRENT_PAGE--;
+                        updateResultsDisplay();
+                    },
+                    componentFactory.getButtonWidth() / 3, 
+                    componentFactory.getButtonHeight() / 2
+                    );
+        } else if ("down".equals(direction)) {
+            return componentFactory.createButton(
+                    LanguagePeer.getText("button.pageDown"), 
+                    () -> {
+                        for (Button button : currentButtons) {
+                            resultPanels.get(CURRENT_PAGE_BACKEND).removeComponent(button);
+                        }
+                        currentButtons.clear();
+                        CURRENT_PAGE++;
+                        updateResultsDisplay();
+                    },
+                    componentFactory.getButtonWidth() / 3, 
+                    componentFactory.getButtonHeight() / 2
+                    );
+        } else if ("back".equals(direction)) {
+            return componentFactory.createButton(
+                    LanguagePeer.getText("button.back"), 
+                    () -> windowManager.returnToMainMenu(window),
+                    componentFactory.getButtonWidth() / 3, 
+                    componentFactory.getButtonHeight() / 2
+                    );
+        } else {
+            throw new IllegalArgumentException("Invalid direction: " + direction);
+        }
+    }
 
-    private void handleSongRendering(Set<Button> currentButtons) {
+    private void handleSongRendering() {
         RetrievedStorage results = backend.getLikedSongs();
         if (results == null || results.size() < 1) {
             Logger.debug("No liked songs found.");
         }
 
+        for (int i = 0; i < results.size() / 20; i++) {
+            Panel panel = new Panel();
+            panel.setLayoutManager(new GridLayout(1));
+            panel.setPreferredSize(new TerminalSize(RESULT_PANEL_WIDTH, RESULT_PANEL_HEIGHT));
+            panel.setFillColorOverride(TextColor.ANSI.BLACK_BRIGHT);
+            resultPanels.add(panel);
+        }
+
         resultsToButtons(results);
-        updateResultsDisplay(currentButtons);
+        updateResultsDisplay();
         Logger.debug("Display has been updated with {} liked songs.", results.size());
     }
 
@@ -116,11 +184,14 @@ public class LikedMusicPage extends BasePage {
                         song.getSongArtist(),
                         song.getDuration());
                 Logger.debug("Like : {}", formattedText);
-                likedMusicButtons.put(i, componentFactory.createButton(
+                for (int j = 0; j < 25; j++) {
+                // likedMusicButtons.put(i, componentFactory.createButton(
+                likedMusicButtons.put(j, componentFactory.createButton(
                             formattedText, 
                             () -> handleSongSelection(song),
                             SONG_BUTTON_WIDTH,
                             SONG_BUTTON_HEIGHT));
+                }
             } catch (Exception e) {
                 Writer buffer = new StringWriter();
                 PrintWriter printWriter = new PrintWriter(buffer);
@@ -131,16 +202,33 @@ public class LikedMusicPage extends BasePage {
         }
     }
 
-    private void updateResultsDisplay(Set<Button> currentButtons) {
+    private void updateResultsDisplay() {
         guiThread.invokeLater(() -> {
-            for (Button button : currentButtons) {
-                resultsBox.removeComponent(button);
+            List<Button> currentButtonList = new ArrayList<>(currentButtons);
+            for (int i = 1; i <= resultPanels.size(); i++) {
+                for (int j = 0; j < currentButtonList.size(); j++) {
+                    if (j >= i * 20) {
+                        break;
+                    }
+                    resultPanels.get(i - 1).removeComponent(currentButtonList.get(j));
+
+                }
             }
             currentButtons.clear();
 
+            int songButtonsAdded = 0;
             for (Map.Entry<Integer, Button> entry : likedMusicButtons.entrySet()) {
-                if (currentButtons.add(entry.getValue())) {
-                    resultsBox.addComponent(entry.getValue());
+                Button button = entry.getValue();
+                if (currentButtons.add(button)) {
+                    Logger.debug("[*] Panel list size: {}, CURRENT_PAGE_BACKEND: {}", resultPanels.size(), CURRENT_PAGE_BACKEND);
+                    resultPanels.get(CURRENT_PAGE_BACKEND).addComponent(button);
+                    songButtonsAdded++;
+                    if (songButtonsAdded >= 20) {
+                        CURRENT_PAGE_BACKEND++;
+                        if (CURRENT_PAGE_BACKEND >= resultPanels.size()) {
+                            resultPanels.add(new Panel());
+                        }
+                    }
                 }
             }
 
